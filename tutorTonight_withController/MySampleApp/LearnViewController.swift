5dfd00bceeccf9a3cab3bd4348e6a9eea0207f1c
Arzaan Irani <arzaan92@hotmail.com>
2016-11-16 13:53:01 -0500

    Search implemented

--- tutorTonight_withController/MySampleApp/LearnViewController.swift
+++ tutorTonight_withController/MySampleApp/LearnViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class LearnViewController: UIViewController {
 
     @IBOutlet weak var SearchField: UITextField!
@@ -22,71 +23,17 @@ class LearnViewController: UIViewController {
     @IBOutlet weak var ToTime: UILabel!
     
     
+ 
+    var i = 0
+    var Lessons = [String]()
+    
     @IBAction func RequestAction(sender: AnyObject) {
         
         
-        //performSegueWithIdentifier("request", sender: self)
         
+        SearchJSON()
         
-        
-        
-        let alert = UIAlertController(title: "1 hour", message: "This session is going to cost \(CostLabel.text!) \n Are you sure you want to proceed?", preferredStyle: .Alert)
-        
-        
-        
-        
-        
-        alert.addAction(UIAlertAction(title: "YES", style: .Default, handler: { action in
-            switch action.style{
-                
-            case .Default:
-                
-                //self.performSegueWithIdentifier("cancel", sender: self)
-                
-                _ = self.tabBarController?.selectedIndex = 0
-                
-                
-            case .Cancel:
-                print("cancel")
-                
-            case .Destructive:
-                print("destructive")
-            }
-        }))
-        
-        
-        
-        alert.addAction(UIAlertAction(title: "NO", style: .Cancel, handler: { (action: UIAlertAction!) in
-            
-            alert .dismissViewControllerAnimated(true, completion: nil)
-            
-            
-        }))
-        
-        
-        User_Name = "Path Morin"
-        Course_Name = "\(SearchField.text!)"
-        Course_Date =  "\(FromDate.text!)"
-        From = "\(FromTime.text!)"
-        To = "\(ToTime.text!)"
-        UpcomingPrice = "\(CostLabel.text!)"
-        
-        
-        
-        
-//        NSUserDefaults.standardUserDefaults().setValue("Path Morin", forKey: "user")
-//        NSUserDefaults.standardUserDefaults().setValue("\(SearchField.text!)", forKey: "coursename")
-//        NSUserDefaults.standardUserDefaults().setValue("\(FromDate.text!)", forKey: "fromDate")
-//        NSUserDefaults.standardUserDefaults().setValue("\(FromTime.text!)", forKey: "fromTime")
-//        NSUserDefaults.standardUserDefaults().setValue("\(ToTime.text!)", forKey: "toTime")
-//        NSUserDefaults.standardUserDefaults().setValue("\(CostLabel.text!)", forKey: "price")
-        
-        
-        
-        self.presentViewController(alert, animated: true, completion: nil)
-        
-        
-        
+ 
         
     }
     
@@ -132,6 +79,104 @@ class LearnViewController: UIViewController {
     
     
     
+    func SearchJSON() {
+        
+        
+        
+        
+        
+        let path: String = NSBundle.mainBundle().pathForResource("Appointments", ofType: "json") as String!
+        
+        
+        let data = NSData(contentsOfFile: path) as NSData!
+        
+        
+        let readableJSON = JSON(data: data, options: NSJSONReadingOptions.MutableContainers, error: nil)
+        
+        
+        
+        while readableJSON["\(i)"] != nil {
+            
+            
+            let LessonsResult = LessonData(Stars: readableJSON["\(i)"]["stars"].string!, Price: readableJSON["\(i)"]["price"].string!, UserName: readableJSON["\(i)"]["user"].string!, CourseName: readableJSON["\(i)"]["course_name"].string!, Date: readableJSON["\(i)"]["date"].string!, FromTime: readableJSON["\(i)"]["from_time"].string!, ToTime: readableJSON["\(i)"]["to_time"].string!)
+            
+            
+            Lessons.append(LessonsResult.CourseName)
+            
+            
+            i += 1
+        }
+        
+        
+        if Lessons.contains("\(SearchField.text!)") {
+        
+        
+            
+            
+            let alert = UIAlertController(title: "1 hour", message: "This session is going to cost \(CostLabel.text!) \n Are you sure you want to proceed?", preferredStyle: .Alert)
+            
+            
+            
+            
+            
+            alert.addAction(UIAlertAction(title: "YES", style: .Default, handler: { action in
+                switch action.style{
+                    
+                case .Default:
+                    
+                    //self.performSegueWithIdentifier("cancel", sender: self)
+                    
+                    _ = self.tabBarController?.selectedIndex = 0
+                    
+                    
+                case .Cancel:
+                    print("cancel")
+                    
+                case .Destructive:
+                    print("destructive")
+                }
+            }))
+            
+            
+            
+            alert.addAction(UIAlertAction(title: "NO", style: .Cancel, handler: { (action: UIAlertAction!) in
+                
+                alert .dismissViewControllerAnimated(true, completion: nil)
+                
+                
+            }))
+            
+            
+            User_Name = "Path Morin"
+            Course_Name = "\(SearchField.text!)"
+            Course_Date =  "\(FromDate.text!)"
+            From = "\(FromTime.text!)"
+            To = "\(ToTime.text!)"
+            UpcomingPrice = "\(CostLabel.text!)"
+            
+            
+            
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        
+        
+        }else {
+        
+            
+
+            
+            let alert = UIAlertController(title: "Alert", message: "The Course You've Entered Does Not Exist", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+
+        
+        }
+        
+        
+        
+        
+    }
     
     func SetFieldsANDButtons() {
     
