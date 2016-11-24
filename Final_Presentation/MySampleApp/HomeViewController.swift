0776efa7039e730a146c265217fe54987dbbb7bb
Atam Singh <atamjeetsingh@cmail.carleton.ca>
2016-11-24 14:47:13 -0500

    First Presentation
    # Conflicts:
    #	tutorTonight_withController/.DS_Store
    #	tutorTonight_withController/MySampleApp/.DS_Store
    #
    tutorTonight_withController/MySampleApp/MySampleApp.xcodeproj/project.pb
    xproj
    #
    tutorTonight_withController/MySampleApp/MySampleApp.xcodeproj/project.xc
    workspace/xcuserdata/Sunny.xcuserdatad/UserInterfaceState.xcuserstate
    #	tutorTonight_withController/MySampleApp/MySampleApp/.DS_Store
    #
    tutorTonight_withController/MySampleApp/MySampleApp/AmazonAws/.DS_Store
    #	tutorTonight_withController/MySampleApp/MySampleApp/App/.DS_Store
    #
    tutorTonight_withController/MySampleApp/MySampleApp/App/Images.xcassets/
    .DS_Store
    #	tutorTonight_withController/MySampleApp/MySampleApp/App/Launch
    Screen.storyboard
    #
    tutorTonight_withController/MySampleApp/MySampleApp/App/Main.storyboard
    #	tutorTonight_withController/MySampleApp/MySampleApp/Sdk/.DS_Store

---
+++ Final_Presentation/MySampleApp/HomeViewController.swift
@@ -0,0 +1,325 @@
+//
+//  HomeViewController.swift
+//  MySampleApp
+//
+//  Created by Arzaan Irani on 2016-11-16. 
+//
+//
+
+import UIKit
+
+var User_Name = ""
+var Course_Name = ""
+var Course_Date = ""
+var From = ""
+var To = ""
+var UpcomingPrice = ""
+
+
+
+var heightC: NSLayoutConstraint!
+
+extension UIViewController {
+    
+    func hideKeyboardWhenTappedAround() {
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        
+    }
+    
+    func dismissKeyboard() {
+        
+        view.endEditing(true)
+    }
+}
+
+
+var UserName = LoginViewController.myGlobals.globalFirstName
+
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    var i = 0
+    
+    var Lessons:[LessonData] = [LessonData]()
+    
+    
+    @IBOutlet weak var tableViewHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    @IBOutlet weak var TopView: UIView!
+    @IBOutlet weak var Welcome: UILabel!
+    @IBOutlet weak var MessageLabel: UILabel!
+    
+    @IBOutlet weak var UpcomingTitle: UILabel!
+   
+    
+    @IBOutlet weak var PreviousTitle: UILabel!
+    
+    
+    @IBOutlet weak var PreviousAppTableView: UITableView!
+    @IBOutlet weak var UpcomingAppTableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+        SettingLabelsANDViews()
+        ParseJSON()
+        
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            super.updateViewConstraints()
+            self.tableViewHeightConstraint?.constant = self.PreviousAppTableView.contentSize.height + 40
+        })
+        
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            super.updateViewConstraints()
+            self.tableViewHeightConstraint?.constant = self.UpcomingAppTableView.contentSize.height + 40
+        })
+    }
+    
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated) // No need for semicolon
+        
+        self.UpcomingAppTableView.reloadData()
+        
+    }
+    
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+    
+    func SettingLabelsANDViews() {
+    
+        
+        
+    
+        scrollView.contentSize = CGSize(width: self.scrollView.frame.size.width, height: 1000.0)
+    
+        PreviousAppTableView.delegate = self
+        PreviousAppTableView.dataSource = self
+        
+        
+        
+        //TopView.backgroundColor = UIColor.whiteColor()
+        Welcome.text = "Good Morning \(UserName)!"
+        MessageLabel.text = "Ready To Learn Something New Today?"
+        
+        UpcomingTitle.text = "Upcoming Appointments"
+        
+        
+        
+        PreviousTitle.text = "Previous Appointments"
+        
+        
+
+    
+    
+    }
+    
+    
+    func ParseJSON() {
+
+        let path: String = NSBundle.mainBundle().pathForResource("Appointments", ofType: "json") as String!
+        
+        
+        let data = NSData(contentsOfFile: path) as NSData!
+       
+    
+        let readableJSON = JSON(data: data, options: NSJSONReadingOptions.MutableContainers, error: nil)
+                
+        
+        
+        while readableJSON["\(i)"] != nil {
+        
+        
+            let LessonsResult = LessonData(Stars: readableJSON["\(i)"]["stars"].string!, Price: readableJSON["\(i)"]["price"].string!, UserName: readableJSON["\(i)"]["user"].string!, CourseName: readableJSON["\(i)"]["course_name"].string!, Date: readableJSON["\(i)"]["date"].string!, FromTime: readableJSON["\(i)"]["from_time"].string!, ToTime: readableJSON["\(i)"]["to_time"].string!)
+            
+            
+            Lessons.append(LessonsResult)
+            
+            self.PreviousAppTableView.reloadData()
+        
+            
+          i += 1
+        }
+        
+        
+        
+        
+    
+    
+    }
+    
+    
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        
+        if tableView == self.PreviousAppTableView {
+        
+        return Lessons.count
+            
+            
+        }else {
+        
+        return 1
+        
+        }
+        
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        
+        return 110
+        
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+        if tableView == self.PreviousAppTableView {
+            
+            
+            
+        let cell = tableView.dequeueReusableCellWithIdentifier("previousApp", forIndexPath: indexPath) as! previousAppTableViewCell
+       
+        
+        //cell.backgroundColor = UIColor.clearColor()
+        
+        
+        let User_Name = Lessons[indexPath.row].UserName
+        let Course_Name = Lessons[indexPath.row].CourseName
+        let Course_Date = Lessons[indexPath.row].Date
+        let From = Lessons[indexPath.row].FromTime
+        let To = Lessons[indexPath.row].ToTime
+        let Price = Lessons[indexPath.row].Price
+        let Stars = Lessons[indexPath.row].Stars
+        
+        
+        
+        
+        cell.LessonDetail.text = "Lesson With \(User_Name) for \(Course_Name) \n on \(Course_Date) from \(From) to \(To)"
+        cell.PriceLabel.text = "$ \(Price)"
+        
+        
+        
+        
+        
+        
+        
+        if Stars == "1" {
+            
+            
+            cell.Star1.image = UIImage(named: "Star-ON")
+            
+            
+        }else if Stars == "2" {
+            
+            
+            cell.Star1.image = UIImage(named: "Star-ON")
+            cell.Star2.image = UIImage(named: "Star-ON")
+            
+            
+        }else if Stars == "3" {
+            
+            
+            cell.Star1.image = UIImage(named: "Star-ON")
+            cell.Star2.image = UIImage(named: "Star-ON")
+            cell.Star3.image = UIImage(named: "Star-ON")
+            
+            
+        }else if Stars == "4" {
+            
+            
+            cell.Star1.image = UIImage(named: "Star-ON")
+            cell.Star2.image = UIImage(named: "Star-ON")
+            cell.Star3.image = UIImage(named: "Star-ON")
+            cell.Star4.image = UIImage(named: "Star-ON")
+            
+            
+        }else {
+            
+            
+            cell.Star1.image = UIImage(named: "Star-ON")
+            cell.Star2.image = UIImage(named: "Star-ON")
+            cell.Star3.image = UIImage(named: "Star-ON")
+            cell.Star4.image = UIImage(named: "Star-ON")
+            cell.Star5.image = UIImage(named: "Star-ON")
+            
+            
+        }
+        
+        
+        
+        
+        return cell
+    
+        }else {
+        
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("upcomingApp", forIndexPath: indexPath) as! upcomingAppTableViewCell
+        
+            
+            
+//            
+//            if NSUserDefaults.valueForKey("price") != nil {
+//            
+//            cell.PriceLabel.text = "\(NSUserDefaults.valueForKey("price"))"
+//                
+//                
+//            }else {
+//            
+//                cell.PriceLabel.text = ""
+//                
+//            
+//            }
+            
+            
+        
+        
+            cell.PriceLabel.text = UpcomingPrice
+            cell.LessonDetail.text = "Lesson With \(User_Name) for \(Course_Name) \n on \(Course_Date) from \(From) to \(To)"
+        
+        
+        
+        
+        
+        
+        return cell
+        
+        
+        }
+    }
+    
+    
+    
+    
+    
+
+}
---
+++ Final_Presentation/MySampleApp/SignUpViewController.swift
@@ -0,0 +1,205 @@
+//
+//  SignUpViewController.swift
+//  MySampleApp
+//
+//  Created by Satinder Singh on 2016-11-12.
+//
+//
+
+import Foundation
+import UIKit
+import AWSMobileHubHelper
+
+class SignUpViewController: UIViewController {
+    
+    
+    @IBOutlet weak var firstNameBox: UITextField!
+    @IBOutlet weak var lastNameBox: UITextField!
+    @IBOutlet weak var emailBox: UITextField!
+    @IBOutlet weak var passwordBox: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: Selector("hideKeyboard"))
+        tapGesture.cancelsTouchesInView = true
+        self.view.addGestureRecognizer(tapGesture)
+        
+    }
+    
+    func hideKeyboard()
+    {
+        self.view.endEditing(true)
+    }
+    
+    
+    func keyboardWillShow(notification: NSNotification) {
+        
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+            if view.frame.origin.y == 0{
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+            else {
+                
+            }
+        }
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+            if view.frame.origin.y != 0 {
+                self.view.frame.origin.y += keyboardSize.height
+            }
+            else {
+                
+            }
+        }
+    }
+    
+    // CALL AWS GIVEN JSON
+    @IBAction func signUpButtonPress(sender: AnyObject) {
+        
+
+        if firstNameBox.text!.characters.count < 2  ||
+            lastNameBox.text!.characters.count < 2  ||
+               emailBox.text!.characters.count < 10 ||
+            passwordBox.text!.characters.count < 8 {
+            //POP UP INVALID
+            let alert = UIAlertView()
+            alert.title = "Error"
+            alert.message = "Please enter valid credentials"
+            alert.addButtonWithTitle("Ok")
+            alert.show()
+        }
+        
+        
+        /* TEST
+         {
+             "callType"  : "PUT",
+             "object"    : "USERS",
+             "data"      : {
+                "username"  : "atamsingh",
+                "password"  : "Compsci3004!",
+                "email"     : "atam_singh@hotmail.com",
+                "firstName" : "Atamjeet",
+                "lastName"  : "Singh",
+                "studentStatus" : false,
+                "tutorStatus"   : false
+             }
+         }
+         */
+        
+        
+        let firstName = firstNameBox.text!
+        let lastName = lastNameBox.text!
+        let email = emailBox.text!
+        let password = passwordBox.text!
+        let studentStatus = true
+        let tutorStatus = false
+        
+
+        let inputData: String = "{\"callType\"  : \"PUT\",\"object\"    : \"USERS\",\"data\"      : {\"username\"  : \"\(email)\",\"password\"  : \"\(password)\",\"email\"  : \"\(email)\",\"firstName\"  : \"\(firstName)\",\"lastName\"  : \"\(lastName)\",\"studentStatus\"  : true, \"tutorStatus\"  : false}}"
+        let functionName = "mainController"
+        let jsonInput = inputData.makeJsonable()
+        let jsonData = jsonInput.dataUsingEncoding(NSUTF8StringEncoding)!
+        var parameters: [String: AnyObject]
+        do {
+            let anyObj = try NSJSONSerialization.JSONObjectWithData(jsonData, options: []) as! [String: AnyObject]
+            parameters = anyObj
+        } catch let error as NSError {
+            print("json error: \(error.localizedDescription)")
+            return
+        }
+        print("Json Input: \(jsonInput)")
+
+        
+        
+        // RETURN FRO AWS LAMBDA
+        AWSCloudLogic.defaultCloudLogic().invokeFunction(functionName, withParameters: parameters, completionBlock: {(result: AnyObject?, error: NSError?) -> Void in
+            if let result = result {
+                dispatch_sync(dispatch_get_main_queue(), {
+                    
+                    
+                    //RETREIVE JSON OBJECT FROM AWS
+                    let NSjsonStr = result as! NSString;
+                    let NSdataStr = NSjsonStr.dataUsingEncoding(NSUTF8StringEncoding)!;
+                    do {
+                        let jsonArray: NSDictionary = try NSJSONSerialization.JSONObjectWithData(NSdataStr, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                        
+                        //                        print("jsonArray: \(jsonArray)")
+                        
+                        let status = jsonArray["status"]! as! String
+                        let info = jsonArray["info"]!
+                        
+                        
+//                        FAILED SIGN UP
+//                            "{\"status\":\"fail\",\"info\":{\"reason\":\"password not valid\"}}"
+                        if status == "fail" {
+                            let reason = info["reason"] as String!
+                            
+                            //pop up alert message
+                            let alert = UIAlertView()
+                            alert.title = "Error"
+                            alert.message = reason
+                            alert.addButtonWithTitle("Ok")
+                            alert.show()
+                            
+                        } else {
+                            // LOGIN SUCCESSFUL
+                            
+                            // global variables
+                            LoginViewController.myGlobals.globalUserName = email
+                            LoginViewController.myGlobals.globalUserID = info["userID"] as String!
+                            LoginViewController.myGlobals.globalFirstName = info["firstName"] as String!
+                            LoginViewController.myGlobals.globalLastName = info["lastName"] as String!
+                            LoginViewController.myGlobals.globalEmail = info["email"] as String!
+                            
+                            // next screen
+                            self.performSegueWithIdentifier("signup2main", sender: nil)
+                        }
+                    } catch {
+                        print("Error: \(error)")
+                    }
+                    
+                    
+                })
+            }
+            
+            print("after async")
+            
+            
+            var errorMessage: String
+            if let error = error {
+                if let cloudUserInfo = error.userInfo as? [String: AnyObject],
+                    cloudMessage = cloudUserInfo["errorMessage"] as? String {
+                    errorMessage = "Error: \(cloudMessage)"
+                } else {
+                    errorMessage = "Error occurred in invoking the Lambda Function. No error message found."
+                }
+                dispatch_async(dispatch_get_main_queue(), {
+                    print("Error occurred in invoking Lambda Function: \(error)")
+                    //Do something with error message
+                    let alertView = UIAlertController(title: NSLocalizedString("Error", comment: "Title bar for error alert."), message: error.localizedDescription, preferredStyle: .Alert)
+                    alertView.addAction(UIAlertAction(title: NSLocalizedString("Dismiss", comment: "Button on alert dialog."), style: .Default, handler: nil))
+                    self.presentViewController(alertView, animated: true, completion: nil)
+                })
+            }
+        })
+        
+    }
+}
+
+
+extension String {
+    private func makeJsonable() -> String {
+        let resultComponents: NSArray = self.componentsSeparatedByCharactersInSet(NSCharacterSet.newlineCharacterSet())
+        return resultComponents.componentsJoinedByString("")
+    }
+}
